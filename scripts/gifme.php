0982a5526911be03c56e917e181fd54f7c0f2d71
David Mays <dave@davidmays.com>
2014-03-03 13:20:06 -0500

    making gifbot explain itself

--- scripts/gifme.php
+++ scripts/gifme.php
@@ -13,7 +13,7 @@ $iconurl = null;
 
 $enc = urlencode($command->Text);
 
-$imageSearchJson = get_url_contents('http://ajax.googleapis.com/ajax/services/search/images?v=1.0&safe=active&as_filetype=gif&rsz=8&imgsz=medium&q=animated+gif+'.$enc);
+$imageSearchJson = get_url_contents('http://ajax.googleapis.com/ajax/services/search/images?v=1.0&safe=active&as_filetype=gif&rsz=8&imgsz=medium&q=animated+'.$enc);
 
 $imageresponse = json_decode($imageSearchJson);
 
@@ -21,7 +21,9 @@ $whichImage = rand(0,7);
 
 $returnedimageurl = $imageresponse->responseData->results[$whichImage]->unescapedUrl;
 
-$ret = slack_incoming_hook_post($hook, "gifbot", $command->ChannelName, $iconurl, $emoji, "@" .$command->UserName."\n".$returnedimageurl);
+$payload = "<https://cim.slack.com/team/{$command->UserName}|{$command->UserName}> asked for '{$command->Text}'\n{$returnedimageurl}";
+
+$ret = slack_incoming_hook_post($hook, "gifbot", $command->ChannelName, $iconurl, $emoji, $payload);
 if($ret!="ok")
 	print_r("@tdm, gifbot got this response when it tried to post to the incoming hook.\n{$ret}");
 ?>
