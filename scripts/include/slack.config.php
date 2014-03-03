3922bab9f219a0f59873da80c9d869fe5024fca6
David Mays <dave@davidmays.com>
2014-03-03 18:54:07 -0500

    Update slack.config.php

--- scripts/include/slack.config.php
+++ scripts/include/slack.config.php
@@ -1,7 +1,5 @@
 <?
 $config['slack']['incominghook'] = "https://cim.slack.com/services/hooks/incoming-webhook?token=";
 
-$config['slack']['incominghooktoken'] = "REPLACE ME";
-
 $config['slack']['hook'] = $config['slack']['incominghook'].$config['slack']['incominghooktoken'];
 ?>
