a4a4c8272f2c2976fb8f1a88fea5bff5f5eea22d
David Mays <dave@davidmays.com>
2014-03-04 10:34:33 -0500

    Update meme.php

--- scripts/meme.php
+++ scripts/meme.php
@@ -15,12 +15,15 @@ text=googlebot: What is the air-speed velocity of an unladen swallow?
 //meme generator API
 $cmd = BuildSlashCommand($_REQUEST);
 
+
+slack_incoming_hook_post($config['slack']['hook'], $cmd->UserName, $cmd->ChannelNAme, null, ":bow:", $cmd->Text);
+die;
 //str_replace ( mixed $search , mixed $replace , mixed $subject [, int &$count ] )
 
 print_r($cmd->Text);die;
 
 $cmdText = $cmd->Text;
-$memetext =  str_replace("memebot:", "", $cmdText, 1);
+$memetext = str_replace("memebot:", "", $cmdText, 1);
 
 $parts = explode("/", $memetext);
 
