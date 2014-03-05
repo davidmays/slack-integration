bc1bf9e29390da9c8a19b10f03f74353db8ee3f6
David Mays <dave@davidmays.com>
2014-03-05 17:29:07 -0500

    added a retry for 503s from google image search

--- scripts/imageme.php
+++ scripts/imageme.php
@@ -10,23 +10,26 @@ $hook = $config['slack']['hook'];
 //use one or the other of $emoji or $iconurl
 $emoji = ":camera:";
 $iconurl = null;
-
-$enc = urlencode($command->Text);
-
-$imageSearchJson = get_url_contents('http://ajax.googleapis.com/ajax/services/search/images?v=1.0&safe=active&rsz=8&imgsz=medium&q='.$enc);
-
-$imageresponse = json_decode($imageSearchJson);
-
-
-
-
-
 $userlink = "<https://cim.slack.com/team/{$command->UserName}|{$command->UserName}>";
+$maxtries = 2;
+$tries = 0;
+
+
+startover:
+    
+$imageresponse = RunImageSearch($command->Text);
+$tries++;
 
 if($imageresponse->responseData == null){
 	//{"responseData": null, "responseDetails": "qps rate exceeded", "responseStatus": 503}
 	$details = $imageresponse->responseDetails;
 	$status = $imageresponse->responseStatus;
+	
+	if($status == 503 && $tries < $maxtries)
+	{
+	    sleep(1);
+	    goto startover; //yeah, it's a goto. deal with it.
+	}
 	
 	print_r("Sorry @{$userlink}, no image for you! [{$details}:{$status}]\n");
 	//print_r($imageresponse);
@@ -41,4 +44,17 @@ $payload = "@{$userlink} asked for '{$command->Text}'\n{$returnedimageurl}";
 $ret = slack_incoming_hook_post($hook, "imagebot", $command->ChannelName, $iconurl, $emoji, $payload);
 if($ret!="ok")
 	print_r("@tdm, gifbot got this response when it tried to post to the incoming hook for /imageme.\n{$ret}");
+	
+	
+	
+function RunImageSearch($text)
+{
+	$enc = urlencode($text);
+
+    $imageSearchJson = get_url_contents('http://ajax.googleapis.com/ajax/services/search/images?v=1.0&safe=active&rsz=8&imgsz=medium&q='.$enc);
+
+    $imageresponse = json_decode($imageSearchJson);
+    
+    return $imageresponse;
+}
 ?>
