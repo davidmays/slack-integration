b2899b0086fbd1b597fa320b35df331d93098f57
jpklein <jp.klein@me.com>
2014-09-04 00:54:54 -0400

    Remove define-like variables from config file

--- scripts/config/default.config.php
+++ scripts/config/default.config.php
@@ -16,6 +16,3 @@ $config['rally']['password'] = "REPLACE ME";
 
 $config['rally']['botname'] = "rallybot";
 $config['rally']['boticon'] = "https://yt3.ggpht.com/-vkXOTHhRGck/AAAAAAAAAAI/AAAAAAAAAAA/IBjv0oYIm5Q/s100-c-k-no/photo.jpg";
-
-$config['rally']['artifactquery'] = "https://rally1.rallydev.com/slm/webservice/v2.0/artifact?query=(FormattedID%20=%20[[ID]])";
-$config['rally']['defectquery'] = "https://rally1.rallydev.com/slm/webservice/v2.0/defect?query=(FormattedID%20=%20[[ID]])";
