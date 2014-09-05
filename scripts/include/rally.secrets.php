c95dd79cae004ad4a12b30536cbd8f0bce0745cb
jpklein <jp.klein@me.com>
2014-09-05 04:04:53 -0400

    Fix includes and define pseudoconstants in cron script

--- scripts/include/rally.secrets.php
+++ scripts/include/rally.secrets.php
@@ -1,4 +1,5 @@
 <?
 $config['rally']['username'] = "REPLACE ME";
 $config['rally']['password'] = "REPLACE ME";
+$RALLY_PROJECT_ID = REPLACE_ME;
 ?>
--- scripts/rallycron.php
+++ scripts/rallycron.php
@@ -2,12 +2,14 @@
 require('include/curl.php');
 require('include/slack.php');
 require('include/rally.php');
-require('config/config.php');
+require('include/rallyme.config.php');
+require('include/slack.config.php');
 
 date_default_timezone_set('UTC');
 
-$config['cron_interval'] = 61; //seconds between cron runs; pad for script run time and latency
-$since = date($config['rally']['date_format'], time() - $config['cron_interval']);
+$CRON_INTERVAL = 61; //seconds between cron runs; pad for script run time and latency
+
+$since = date($RALLY_TIMESTAMP_FORMAT, time() - $CRON_INTERVAL);
 
 $items = FetchLatestRallyItems($since);
 $result = SendRallyNotifications($items);
