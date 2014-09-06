b732545e466b2d8d3046542fd2a40eb6ba887296
jpklein <jp.klein@me.com>
2014-09-06 10:43:01 -0400

    Add settings for rally cron job

---
+++ scripts/config/rallycron.conf.php
@@ -0,0 +1,4 @@
+<?php
+$CRON_INTERVAL = 61; //seconds between cron runs; pad for script run time and latency
+$RALLY_PROJECT_ID = REPLACE_ME;
+$SLACK_CHANNEL_FOR_RALLY_PROJECT = 'REPLACE ME'; //do not include hash symbol
