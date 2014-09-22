5010c3052cfb692123d9624db49504fffe410f41
JP Klein <jp.klein@nbcuni.com>
2014-09-22 15:07:49 -0400

    Fix bug introduced in a9ffe3e (merge branch issue4)

--- scripts/rallycron.php
+++ scripts/rallycron.php
@@ -5,6 +5,8 @@ require('include/rallycron.inc.php');
 date_default_timezone_set('UTC');
 $since = date($RALLY_TIMESTAMP_FORMAT, time() - $CRON_INTERVAL);
 
+$result = NULL;
+
 if ($items = FetchLatestRallyComments($since)) {
 	$result = SendRallyCommentNotifications($items);
 }
