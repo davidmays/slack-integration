a6e5d7e168964b32c4a902aba86eeb88ea411689
JP Klein <jp.klein@nbcuni.com>
2014-09-22 14:08:01 -0400

    Re-engineer HTML-request code for Google App Engine

--- scripts/include/curl.php
+++ scripts/include/curl.php
@@ -1,42 +1,33 @@
-<?
-//HTTP Utility Methods
+<?php
+//HTTP Utility Methods for Google App Engine's PHP Runtime
 
-function get_url_contents($url) {
-    $crl = curl_init($url);
-
-    curl_setopt($crl, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
-    curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($crl, CURLOPT_CONNECTTIMEOUT, 5);
-
-    $response = curl_exec($crl);
-    curl_close($crl);
-    return $response;
-}
-
-function get_url_contents_with_basicauth($url, $username, $password) {
-    $crl = curl_init();
-
-    curl_setopt($crl, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
-    curl_setopt($crl, CURLOPT_URL, $url);
-	curl_setopt($crl, CURLOPT_USERPWD, "{$username}:{$password}");
-    curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($crl, CURLOPT_CONNECTTIMEOUT, 5);
-
-    $ret = curl_exec($crl);
-    curl_close($crl);
-    return $ret;
-}
-
-function curl_post($uri, $data)
+function get_url_contents($url)
 {
-	$crl = curl_init($uri);
-	curl_setopt($crl, CURLOPT_CUSTOMREQUEST, "POST");
-	curl_setopt($crl, CURLOPT_POSTFIELDS, $data);
-	curl_setopt($crl, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($crl, CURLOPT_HTTPHEADER, array('Content-Length: ' . strlen($data)));
+    $context = array('http' => array('method' => 'get'));
+    $context = stream_context_create($context);
 
-	$response = curl_exec($crl);
-	curl_close($crl);
-	return $response;
+    return file_get_contents($url, FALSE, $context);
 }
-?>
+
+function get_url_contents_with_basicauth($url, $username, $password)
+{
+    $auth_header = $username . ':' . $password;
+    $auth_header = base64_encode($auth_header);
+    $auth_header = 'Authorization: Basic ' . $auth_header . "\r\n";
+
+    $context = array('http' => array('method' => 'get', 'header' => $auth_header));
+    $context = stream_context_create($context);
+
+    return file_get_contents($url, FALSE, $context);
+}
+
+function curl_post($url, $data_string)
+{
+    $length_header = strlen($data_string);
+    $length_header = 'Content-Length: ' . $length_header . "\r\n";
+
+    $context = array('http' => array('method' => 'post', 'header' => $length_header, 'content' => $data_string));
+    $context = stream_context_create($context);
+
+    return file_get_contents($url, FALSE, $context);
+}
