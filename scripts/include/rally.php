56ff06f288482d4ffc46a3379657b4e88149a192
JP Klein <jp.klein@nbcuni.com>
2014-10-01 00:08:16 -0400

    Remove deprecated functions

--- scripts/include/rally.php
+++ scripts/include/rally.php
@@ -4,55 +4,6 @@ $RALLY_BASE_URL = 'https://rally1.rallydev.com/';
 $RALLY_API_URL = $RALLY_BASE_URL . 'slm/webservice/v2.0/';
 
 
-function HandleStory($id, $channel_name)
-{
-	$ref = FindRequirement($id);
-
-	$payload = GetRequirementPayload($ref);
-
-	$result = postit($channel_name, $payload->text, $payload->attachments);
-
-	if($result=='Invalid channel specified'){
-	    die("Sorry, the rallyme command can't post messages to your private chat.\n");
-	}
-
-	if($result!="ok"){
-		print_r($result."\n");
-		print_r(json_encode($payload));
-		print_r("\n");
-		die("Apparently the Rallyme script is having a problem. Ask <https://cim.slack.com/team/tdm|@tdm> about it. :frowning:");
-	}
-	return $result;
-}
-
-function postit($channel_name, $payload, $attachments){
-	global $config, $slackCommand;
-
-	return slack_incoming_hook_post_with_attachments(
-		$config['slack']['hook'],
-		$config['rally']['botname'],
-		$slackCommand->ChannelName,
-		$config['rally']['boticon'],
-		$payload,
-		$attachments);
-}
-
-
-
-function GetRallyAttachmentLink($attachmentRef)
-{
-	$attachments = CallAPI($attachmentRef);
-	$firstattachment = $attachments->QueryResult->Results[0];
-
-	$attachmentname = $firstattachment->_refObjectName;
-	$encodedattachmentname = urlencode($attachmentname);
-	$id = $firstattachment->ObjectID;
-
-	$uri = "https://rally1.rallydev.com/slm/attachment/{$id}/{$encodedattachmentname}";
-	$linktxt = "<{$uri}|{$attachmentname}>";
-	return $linktxt;
-}
-
 function MakeField($title, $value, $short=false)
 {
 	$attachmentfield = array(
@@ -71,26 +22,6 @@ function CallAPI($uri)
 	$object = json_decode($json);
 
 	return $object;
-}
-
-function FindRequirement($id)
-{
-	$query = GetArtifactQueryUri($id);
-
-	$searchresult = CallAPI($query);
-//	print_r($searchresult);die;
-
-	$count = GetCount($searchresult);
-	if($count == 0)
-		NotFound($id);
-
-	return GetFirstObjectFromSearchResult("HierarchicalRequirement", $searchresult);
-}
-
-function GetArtifactQueryUri($id)
-{
-	global $config;
-	return str_replace("[[ID]]", $id, $config['rally']['artifactquery']);
 }
 
 function TruncateText($text, $len)
--- scripts/include/rallyme.config.php
+++ scripts/include/rallyme.config.php
@@ -1,8 +1,6 @@
 <?
 require('rally.secrets.php');
 
-$config['rally']['artifactquery'] = "https://rally1.rallydev.com/slm/webservice/v2.0/artifact?query=(FormattedID%20=%20[[ID]])";
-
 $config['rally']['boticon'] = "https://yt3.ggpht.com/-vkXOTHhRGck/AAAAAAAAAAI/AAAAAAAAAAA/IBjv0oYIm5Q/s100-c-k-no/photo.jpg";
 
 $config['rally']['botname'] = "rallybot";
