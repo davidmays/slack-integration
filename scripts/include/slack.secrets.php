2be464d9f8a291a67c836dbdb11329506cdf979b
Ryan Sprake <ryan.sprake@gmail.com>
2015-10-29 20:16:15 -0400

    Updated to use a Incoming WebHook URL

--- scripts/include/slack.secrets.php
+++ scripts/include/slack.secrets.php
@@ -1,4 +1,4 @@
 <?
-$config['slack']['incominghooktoken'] = "REPLACE ME";
-$config['slack']['outgoinghooktoken'] = "REPLACE ME";
+$config['slack']['incomingwebhookurl'] = "REPLACE ME";  // The WebHook URL from your Incoming WebHook
+$config['slack']['outgoinghooktoken'] = "REPLACE ME";   // The Token from your Outgoing WebHook
 ?>
